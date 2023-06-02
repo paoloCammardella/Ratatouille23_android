c3c0d056ae362d91fea8b51441786e50b9d7828f
paoloCammardella <po.cammardella@gmail.com>
2023-06-02 19:19:41 +0200

    creazione bottom bar e creazione stile bottom bar

--- app/src/main/java/com/example/ratatouille23/MainActivity.java
+++ app/src/main/java/com/example/ratatouille23/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.ratatouille23;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.View;
+
+import com.example.ratatouille23.View.LoginFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomBar;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        bottomBar = findViewById(R.id.bottomBar);
+        bottomBar.setVisibility(View.VISIBLE);
     }
 }
--- app/src/main/java/com/example/ratatouille23/View/LoginFragment.java
+++ app/src/main/java/com/example/ratatouille23/View/LoginFragment.java
@@ -1,5 +1,7 @@
 package com.example.ratatouille23.View;
 
+import static android.view.View.VISIBLE;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,11 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ratatouille23.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
 
 public class LoginFragment extends Fragment {
 
     MaterialButton loginBtn;
+    BottomNavigationView bottomBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +33,10 @@ public class LoginFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
         loginBtn = view.findViewById(R.id.loginButton);
+//        bottomBar.
         loginBtn.setOnClickListener(view1 -> {
             Navigation.findNavController(view).navigate(R.id.homeFragment);
+            bottomBar.setVisibility(VISIBLE);
         });
 
         return view;
